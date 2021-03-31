43ebacec88f879f2d04c8c2f0bb8aa88a58f1fcd
Diego <burgosd026@yahoo.com>
2021-03-31 18:59:38 -0500

    deployment with yarn v1

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ require('./config/database');
 // Mount Middleware:
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static('public'));
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
 
 // Mount API Routes:
 app.use('/api/users', require('./routes/api/users'));
